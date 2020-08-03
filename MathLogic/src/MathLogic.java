fafeb0e2eda1c07634baa5343672a7faa9117adc
Jeff Somers <somersj@amazon.com>
2020-08-03 16:51:42 -0400

    Revert "Do exponentiation"
    This reverts commit 3c4ef7e9

--- MathLogic/src/MathLogic.java
+++ MathLogic/src/MathLogic.java
@@ -1,9 +1,6 @@
 public class MathLogic {
 
     public static void main(String[] args) {
-        double value = 3.0f;
-        double exponent = 4.0f;
-        double result = Math.pow(value, exponent);
-        System.out.println("result is " + result);
+        System.out.println("testing 123 again");
     }
 }
